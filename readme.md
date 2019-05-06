f035be5d10705323fae30d944c2b5f559c6b3276
Morteza Faghih Shojaei <morteza@averta.net>
2019-05-06 22:59:09 +0430

    initial commit

---
+++ readme.md
@@ -0,0 +1,30 @@
+# Puppeteer Full Page Screenshot
+
+[Puppeteer](https://github.com/GoogleChrome/puppeteer) has the option of taking full page screenshot but it doesn't perform correctly if the page is tall or it contains elements that size relatively to the viewport (ie. `height: 100vh`). To find more information about these issues check [here](https://github.com/GoogleChrome/puppeteer/issues/703) and [here](https://github.com/GoogleChrome/puppeteer/issues/359).
+
+You can use this library to take full page screenshots with puppeteer without worrying about above said issues. It takes multiple screenshots internally then merges them.
+
+## Usage
+
+```javascript
+import puppeteer from "puppeteer";
+import fullPageScreenshot from "puppeteer-fullpage-screenshot";
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1920, height: 1080 });
+    await page.goto("http://example.com/");
+
+    await fullPageScreenshot(page, { path: "./page.png" });
+
+    await browser.close();
+})();
+```
+
+> **NOTE** As this library scrolls down to take screenshots, sticky elements may appear multiple times on the final image. In other to fix this issue you need to add custom styles to reset sticky styles just before taking the screenshot.
+
+## Options
+
+-   `delay` - Specifies the delay between each internal screenshot in milliseconds.
+-   `path` - The file path to save the image to.
---
+++ src/index.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-await-in-loop */
+import merge from 'merge-img';
+import Jimp from 'jimp';
+
+const pageDown = async ( page ) => {
+    const isEnd = await page.evaluate( () => {
+        window.scrollBy( 0, window.innerHeight );
+        return window.scrollY >= document.body.clientHeight - window.innerHeight;
+    } );
+
+    return isEnd;
+};
+
+const fullPageScreenshot = async ( page, options = {} ) => {
+    const { pagesCount, extraPixels, viewport } = await page.evaluate( () => {
+        window.scrollTo( 0, 0 );
+        return {
+            pagesCount: Math.ceil( document.body.clientHeight / window.innerHeight ),
+            extraPixels: document.body.clientHeight % window.innerHeight,
+            viewport: { height: window.innerHeight, width: window.innerWidth },
+        };
+    } );
+
+    const images = [];
+    for ( let index = 0; index < pagesCount; index += 1 ) {
+        if ( options.delay ) {
+            await page.waitFor( options.delay );
+        }
+        const image = await page.screenshot( { fullPage: false } );
+        await pageDown( page );
+        images.push( image );
+    }
+
+    // crop last image extra pixels
+    const cropped = await Jimp.read( images.pop() )
+        .then( image => image.crop( 0, viewport.height - extraPixels, viewport.width, extraPixels ) )
+        .then( image => image.getBufferAsync( Jimp.AUTO ) );
+
+    images.push( cropped );
+    const mergedImage = await merge( images, { direction: true } );
+    if ( options.path ) mergedImage.write( options.path );
+
+    return mergedImage.data;
+};
+
+export default fullPageScreenshot;
